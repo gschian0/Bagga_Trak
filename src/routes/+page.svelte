68c2ed36bd6cd45e24d85f8962b79ee3687a48a7
CodeSandbox <support@codesandbox.io>
2022-11-25 22:01:19 +0000

    Initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,65 @@
+<script>
+	import Counter from './Counter.svelte';
+	import welcome from '$lib/images/svelte-welcome.webp';
+	import welcome_fallback from '$lib/images/svelte-welcome.png';
+</script>
+
+<svelte:head>
+	<title>Home</title>
+	<meta name="description" content="Svelte demo app" />
+</svelte:head>
+
+<section>
+<h1> <a href="https://cash.app/$Bagga15/5"><strong>LINK TO CASH APP</strong></a></h1>
+<iframe width="560" height="315" src="https://www.youtube.com/embed/ucFWvtDyVoo" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+<br>
+<iframe width="560" height="315" src="https://www.youtube.com/embed/n6jYFVTZz6s" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+	<!-- <h1>
+		<span class="welcome">
+			<picture>
+				<source srcset={welcome} type="image/webp" />
+				<img src={welcome_fallback} alt="Welcome" />
+			</picture>
+		</span>
+
+		to your new<br />SvelteKit app
+	</h1> -->
+
+	<h2>
+		Support BAGGA TRACK at ANY STAGE from ANYWHERE
+	</h2>
+	<br>
+	
+<!-- 
+	<Counter /> -->
+</section>
+
+<style>
+	section {
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+		flex: 0.6;
+	}
+
+	h1 {
+		width: 100%;
+	}
+
+	.welcome {
+		display: block;
+		position: relative;
+		width: 100%;
+		height: 0;
+		padding: 0 0 calc(100% * 495 / 2048) 0;
+	}
+
+	.welcome img {
+		position: absolute;
+		width: 100%;
+		height: 100%;
+		top: 0;
+		display: block;
+	}
+</style>
---
+++ src/routes/about/+page.svelte
@@ -0,0 +1,25 @@
+<svelte:head>
+	<title>About</title>
+	<meta name="description" content="About this app" />
+</svelte:head>
+
+<div class="text-column">
+<h1> <a href="https://cash.app/$Bagga15/5"><strong>LINK TO CASH APP</strong></a></h1>
+	<h1>- Bagga Trak -</h1>
+
+	<p>
+		A Spirtiual Warrior and Guide for the the world music community.
+		FOLLOW ON ALL CHANNELS AND OUTLETS to support the ART AND MUSIC.
+	</p>
+
+	<pre>Reggae - Rasta - Future Sound</pre>
+
+	<p>
+		He needs your support RIGHT NOW so he can keep delivering the message of Rastafari to the youth of today and anyone willing to hear the truth.
+	</p>
+
+	<p>
+		To Send Money in REAL TIME to support Bagga Track and his mission <a href="/sverdle">Click this link to CASH APP</a> 
+		support starts at 1 dollar. :)
+	</p>
+</div>
